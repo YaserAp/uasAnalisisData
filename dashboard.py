c258324f9a9ef17c55c1bbd3a94c914ff78bdce2
YaserAp <125145424+YaserAp@users.noreply.github.com>
2024-02-15 11:26:37 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,8 +9,19 @@ def load_data(url):
         data = pd.read_csv(url)
         return data
     except Exception as e:
-        st.error(f"Terjadi kesalahan saat memuat data: {e}")
         return None
+
+def main():
+    df_day = load_data('https://github.com/YaserAp/uasAnalisisData/blob/main/day.csv')
+    df_hour = load_data('https://github.com/YaserAp/uasAnalisisData/blob/main/hour.csv')
+
+    if df_day is not None and df_hour is not None:
+        data_2011 = df_hour[df_hour['yr'] == 0]
+        data_2012 = df_hour[df_hour['yr'] == 1]
+
+        create_visualization(data_2011, data_2012)
+    else:
+        st.error("Gagal memuat data. Silakan periksa URL atau coba lagi nanti.")
 
 def create_visualization(data_2011, data_2012):
     peminjaman_2011 = data_2011.groupby(data_2011['dteday'].dt.month)['cnt'].sum()
@@ -25,18 +36,6 @@ def create_visualization(data_2011, data_2012):
     plt.xticks(range(1, 13), ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
     plt.legend()
     st.pyplot()
-
-def main():
-    df_day = load_data('https://github.com/YaserAp/uasAnalisisData/blob/main/day.csv')
-    df_hour = load_data('https://github.com/YaserAp/uasAnalisisData/blob/main/hour.csv')
-
-    if df_day is not None and df_hour is not None:
-        data_2011 = df_hour[df_hour['yr'] == 0]
-        data_2012 = df_hour[df_hour['yr'] == 1]
-
-        create_visualization(data_2011, data_2012)
-    else:
-        st.error("Gagal memuat data. Silakan periksa URL atau coba lagi nanti.")
 
 if __name__ == "__main__":
     main()
